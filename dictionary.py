51669c18b19fa81aae58b1892ca838d53161a8b4
Sanatan kafle <kaflesanatan100@gmail.com>
2019-09-13 17:39:45 +0545

    added remark

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,19 @@
 dictionary={
-	"sanatan":[90,96,70,81,64],
+	"sanatan":[23,96,70,81,64],
 	"sudip":[84,90,100,59,99],
 	"munu":[88,77,99,66,55],
-	"toney":[99,56,78,97,56],
-	"sujan":[76,67,87,78,99]
+	"toney":[99,50,78,97,56],
+	"sujan":[7,67,87,78,99]
 }
 name=input("Enter the name of the student whose percentage you would like to know: ")
+for marks in dictionary[name]:
+	if marks<40:
+		remark="failed"
+		break
+	else:
+		remark="passed"
+
+
 total_marks=sum(dictionary[name])
 percentage=total_marks/500*100
-print("The total marks and percentage obtained by ",name," is",total_marks ," and " ,percentage,"% simultaneously.")
-
+print("The total marks and percentage obtained by ",name," is",total_marks ," and " ,percentage,"% simultaneously and has ",remark)
