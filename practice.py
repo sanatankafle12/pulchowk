3c1d0c71b398fd24b33851bd5732fac22f04510c
Sanatan kafle <kaflesanatan100@gmail.com>
2019-09-13 08:50:03 +0545

    simplified by calculating and printing one time

--- practice.py
+++ practice.py
@@ -9,18 +9,13 @@ if faculty=='computer':
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-	chances=z/7*100
-	print("There is ",chances,'%',' chance that you will get admitted in pulchowk in computer engineering')
-
+	
 elif faculty=='civil':
 	lst_rank=[388,341,282,341,371,377,569]
 	z=0
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-	chances=z/7*100
-	
-	print("There is ",chances,'%',' chance that you will get admitted in pulchowk in civil engineering')
 	
 elif faculty=='electrical':
 	lst_rank=[563,577,389,402,397,387,755]
@@ -28,8 +23,7 @@ elif faculty=='electrical':
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-	chances=z/7*100
-	print("There is ",chances,'%',' chance that you will get admitted in pulchowk in electrical engineering')
+	
 	
 elif faculty=='electronics':
 	lst_rank=[427,504,386,361,387,360,354]
@@ -37,9 +31,7 @@ elif faculty=='electronics':
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-	chances=z/7*100
 
-	print("There is ",chances,'%',' chance that you will get admitted in pulchowk in electronics engineering')
 	
 elif faculty=='architect':
 	lst_rank=[1194,1324,1425,2587,1816,1280,909]
@@ -47,9 +39,6 @@ elif faculty=='architect':
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-	chances=z/7*100
-	
-	print("There is ",chances,'%',' chance that you will get admitted in pulchowk in architect')
 	
 elif faculty=='mechanical':
 	lst_rank=[569,474,312,289,380,264,567]
@@ -57,18 +46,13 @@ elif faculty=='mechanical':
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-	chances=z/7*100
-	print("There is ",chances,'%',' chance that you will get admitted in pulchowk in mechanical engineering')
-	
+		
 elif faculty=='aerospace':
 	lst_rank=[988,683]
 	z=0
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-			
-	Chances=z/2*100
-	print("There is ",Chances,'%',' chance that you will get admitted in pulchowk in aerospace engineering')
 	
 elif faculty=='chemical':
 	lst_rank=[3184]
@@ -76,12 +60,12 @@ elif faculty=='chemical':
 	for i in lst_rank:
 		if i>rank:
 			z+=1
-				
-	Chances=z*100
-	print("There is ",Chances,'%',' chance that you will get admitted in pulchowk in chemical engineering')
 	
 else:
 	print("please be sure that you have chosen correct faculty")
+
+Chances=z/len(lst_rank)*100
+print("There is ",Chances,'%',' chance that you will get admitted in pulchowk in ', faculty ,'engineering')
 
 print("NOTE: This is a rough data from past 7 years (i.e from 2070). Every year, the rank keeps on changing so, this isn't a 100% thing. It has been created just to give you a normal idea.")
 
