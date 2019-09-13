7250164108d7e1f12a9efa162ef354a7adb61a03
Sanatan kafle <kaflesanatan100@gmail.com>
2019-09-13 18:07:34 +0545

    dictionary in pulchowk rank

---
+++ python_dictionary.py
@@ -0,0 +1,23 @@
+dictionary={
+	"computer":[270,308,180,196,224,266,520],
+	"civil":[388,341,282,341,371,377,569],
+	"electronics":[427,504,386,361,387,360,354],
+	"electrical":[563,577,389,402,397,387,755],
+	"architect":[1194,1324,1425,2587,1816,1280,909],
+	"mechanical":[569,474,312,289,380,264,567],
+	"aerospace":[988,683],
+	"chemical":[3184]
+}
+
+faculty=input("Enter the faculty you what to know about: ").lower()
+rank=int(input("Enter your rank: "))
+
+lst_rank=dictionary[faculty]
+no_of_chances=0
+for i in lst_rank:
+	if rank<i:
+		no_of_chances+=1
+percentage=no_of_chances/len(lst_rank)*100
+print("You would have gotten admission ",no_of_chances,"times in ",len(lst_rank),"years in ",faculty," engineering. The Probable percentage being ",percentage,"%.")
+
+
